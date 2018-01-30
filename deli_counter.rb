0047069d577090c4a1e08c2f9b96e24e9808ba0c
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 14:36:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(deli, name)
   deli << name
-  puts "Welcome, #{name}. You are number #{deli.size + 1} in line."
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
 end
 
 take_a_number(katz_deli, "Joe")
